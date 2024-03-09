f2821cabf997b4863f9e2a52e8b6ea320a5b5291
Angelo_braga <128182753+AngeloBraga12@users.noreply.github.com>
2024-03-09 16:51:37 -0300

    feat: config material ui theme and css baseline

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+
+let theme = createTheme({
+    palette: {
+        primary: {
+            main: '#23232',
+        },
+        secondary: {
+            main: '#4f8e3e',
+        },
+    },
+    typography: {
+        fontFamily: "Helvetica Neue"
+    }
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
